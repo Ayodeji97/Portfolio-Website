840d23877cdf8d357d65232db65c7e2eb2745434
Ojo <oluwatobiojo2911@gmail.com>
2024-09-16 22:04:46 +0100

    Add Navbar component for website navigation

---
+++ src/navbar.tsx
@@ -0,0 +1,27 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="container">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
