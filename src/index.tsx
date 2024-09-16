b64590211ace6b9db32f4f1d046922508e6739b3
Ojo <oluwatobiojo2911@gmail.com>
2024-09-16 22:06:05 +0100

    Add Sections to App

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import About from "./about";
+import Contact from "./contact";
+import Experience from "./experience";
+import Hero from "./home";
+import Navbar from "./navbar";
+import Project from "./project";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Project />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
