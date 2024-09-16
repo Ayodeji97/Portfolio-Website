844cc8e8de897c7b13a1796a2b51a7e10900972f
Ojo <oluwatobiojo2911@gmail.com>
2024-09-16 22:05:14 +0100

    Added some placeholder sections

---
+++ src/experience.tsx
@@ -0,0 +1,10 @@
+function Experience() {
+  return (
+    <section>
+      <h1>Experience</h1>
+      <p>Experience page</p>
+    </section>
+  );
+}
+
+export default Experience;
---
+++ src/project.tsx
@@ -0,0 +1,10 @@
+function Project() {
+  return (
+    <div>
+      <h1>Project</h1>
+      <p>Project page</p>
+    </div>
+  );
+}
+
+export default Project;
