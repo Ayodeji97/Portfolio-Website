c480075a9fc4e5bf3690290a1610e4bac00acf9d
Ojo <oluwatobiojo2911@gmail.com>
2024-09-18 22:16:26 +0100

    Add Technology component for displaying technology icons

---
+++ src/components/technology.tsx
@@ -0,0 +1,11 @@
+import { TechnologyIcon } from "../app.interface";
+
+function Technology({ src, alt }: TechnologyIcon) {
+  return (
+    <li className="about-skill">
+      <img src={src} alt={alt} className="technology-icon" />
+    </li>
+  );
+}
+
+export default Technology;
