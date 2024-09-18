4372de96b9916ce4242656748f67205329e6e060
Ojo <oluwatobiojo2911@gmail.com>
2024-09-18 22:14:25 +0100

    Started the experience section of the project

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,8 +1,27 @@
+import { experiences } from "./app.constants";
+import ExperienceComponent from "./components/experience";
+import FullStop from "./components/full-stop";
+
 function Experience() {
   return (
-    <section>
-      <h1>Experience</h1>
-      <p>Experience page</p>
+    <section className="experience-section">
+      <div className="container">
+        <div className="header">
+          {" "}
+          <h1>Experience</h1> <FullStop />
+        </div>
+        <div className="experience-box">
+          {experiences.map((experience, i) => (
+            <ExperienceComponent
+              role={experience.role}
+              companyName={experience.companyName}
+              description={experience.description}
+              index={i + 1}
+              key={i}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
