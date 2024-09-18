18a3c30972b8f43ed52ab60e28ff8f5c5b5b6460
Ojo <oluwatobiojo2911@gmail.com>
2024-09-18 22:13:06 +0100

    Add About page content and technologies/hobbies sections

--- src/about.tsx
+++ src/about.tsx
@@ -1,8 +1,39 @@
+import { technologies } from "./app.constants";
+import FullStop from "./components/full-stop";
+import Technology from "./components/technology";
+
 function About() {
   return (
-    <section>
-      <h1>About</h1>
-      <p>About page</p>
+    <section className="about-section">
+      <div className="container">
+        <div className=" header about-header">
+          {" "}
+          <h1>About me</h1> <FullStop />
+        </div>
+        <div className="about-content">
+          <div className="about-bar"></div>
+          <p className="about-text">
+            I am a software engineer with a passion for web development. I have
+            experience in building web applications using modern technologies.
+          </p>
+        </div>
+        <div className="about-skills">
+          <h3>My Technologies .</h3>
+          <ul className="about-skills-list">
+            {technologies.map((tech) => (
+              <Technology src={tech.src} alt={tech.alt} />
+            ))}
+          </ul>
+        </div>
+        <div className="about-skills">
+          <h3>My Hobbies .</h3>
+          <ul className="about-skills-list">
+            {technologies.map((tech) => (
+              <Technology src={tech.src} alt={tech.alt} />
+            ))}
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
