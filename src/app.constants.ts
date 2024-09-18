8392ee48a228ee5fcef49fdf2bd3162e56ad8bda
Ojo <oluwatobiojo2911@gmail.com>
2024-09-18 22:12:50 +0100

    Add app.constants.ts file for defining data

---
+++ src/app.constants.ts
@@ -0,0 +1,42 @@
+import { ExperienceType, TechnologyIcon } from "./app.interface";
+
+export const technologies: TechnologyIcon[] = [
+  {
+    src: "svg/kotlin.svg",
+    alt: "KOTLIN",
+  },
+  {
+    src: "svg/kotlin.svg",
+    alt: "KOTLIN",
+  },
+  {
+    src: "svg/kotlin.svg",
+    alt: "KOTLIN",
+  },
+  {
+    src: "svg/kotlin.svg",
+    alt: "KOTLIN",
+  },
+  {
+    src: "svg/kotlin.svg",
+    alt: "KOTLIN",
+  },
+];
+
+export const experiences: ExperienceType[] = [
+  {
+    role: "Backend Developer",
+    companyName: "Google",
+    description: "I used backend technologies like node, express and mongo db",
+  },
+  {
+    role: "Backend Developer",
+    companyName: "Google",
+    description: "I used backend technologies like node, express and mongo db",
+  },
+  {
+    role: "Backend Developer",
+    companyName: "Google",
+    description: "I used backend technologies like node, express and mongo db",
+  },
+];
