b98adaf7e5f694ba4ca2c878d4a2f5d67815fcea
Ojo <oluwatobiojo2911@gmail.com>
2024-09-21 22:57:12 +0100

    Added Projects to the constant file

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,4 +1,4 @@
-import { ExperienceType, TechnologyIcon } from "./app.interface";
+import { ExperienceType, ProjectType, TechnologyIcon } from "./app.interface";
 
 export const technologies: TechnologyIcon[] = [
   {
@@ -38,5 +38,45 @@ export const experiences: ExperienceType[] = [
     role: "Backend Developer",
     companyName: "Google",
     description: "I used backend technologies like node, express and mongo db",
+  },
+  {
+    role: "Backend Developer",
+    companyName: "Google",
+    description: "I used backend technologies like node, express and mongo db",
+  },
+];
+
+export const projects: ProjectType[] = [
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, odio et tincidunt fermentum, metus sapien ornare nunc, nec tempus tortor lacus a nulla.",
+    githubLink: "https://github.com",
+    liveLink: "https://github.com",
+    img: "https://via.placeholder.com/300",
+  },
+  {
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, odio et tincidunt fermentum, metus sapien ornare nunc, nec tempus tortor lacus a nulla.",
+    githubLink: "https://github.com",
+    liveLink: "https://github.com",
+    img: "https://via.placeholder.com/300",
+  },
+  {
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, odio et tincidunt fermentum, metus sapien ornare nunc, nec tempus tortor lacus a nulla.",
+    githubLink: "https://github.com",
+    liveLink: "https://github.com",
+    img: "https://via.placeholder.com/300",
+  },
+  {
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, odio et tincidunt fermentum, metus sapien ornare nunc, nec tempus tortor lacus a nulla.",
+    githubLink: "https://github.com",
+    liveLink: "https://github.com",
+    img: "https://via.placeholder.com/300",
   },
 ];
