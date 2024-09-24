771f70cf03921cfd410ccbadd700e9c1e7721edc
Ojo <oluwatobiojo2911@gmail.com>
2024-09-24 14:27:33 +0100

    Refactor contact component to add email icon and social links dynamically

--- src/contact.tsx
+++ src/contact.tsx
@@ -1,9 +1,43 @@
-import { Github } from "./components/icons/github";
-import { LinkedIn } from "./components/icons/linkedin";
-import { Twitter } from "./components/icons/mail";
+import { useEffect } from "react";
+import { TfiEmail } from "react-icons/tfi";
+import { socialLinks } from "./app.constants";
 import FullStop from "./components/ui/full-stop";
+import SocialLink from "./components/ui/social-link";
 
 function Contact() {
+  const handleHover = (value: number, e: MouseEvent) => {
+    if ((e.target as HTMLElement).closest(".social--link")) {
+      const link = (e.target as HTMLElement).closest(
+        ".social--link"
+      ) as HTMLElement;
+      const siblings =
+        link.closest(".social--links")?.querySelectorAll(".social--link") || [];
+      siblings.forEach((el) => {
+        if (el !== link) {
+          (el as HTMLElement).style.opacity = value.toString();
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    const socialLinksEl = document.getElementById("social--links");
+    const mouseOverHandler = (e: MouseEvent) => handleHover(0.5, e);
+    const mouseOutHandler = (e: MouseEvent) => handleHover(1, e);
+
+    if (socialLinksEl) {
+      socialLinksEl.addEventListener("mouseover", mouseOverHandler);
+      socialLinksEl.addEventListener("mouseout", mouseOutHandler);
+
+      return () => {
+        socialLinksEl.removeEventListener("mouseover", mouseOverHandler);
+        socialLinksEl.removeEventListener("mouseout", mouseOutHandler);
+      };
+    }
+  }, []);
+
+  const year = new Date().getFullYear();
+
   return (
     <section className="contact-section">
       <div className="container">
@@ -12,10 +46,11 @@ function Contact() {
           <h1>Get in Touch</h1> <FullStop />
         </div>
         <div className="contact-box">
-          <div className="contact-details">
+          <div className="contact-message">
             <h3>
               Looking to partner or work together? Reach out through the form
-              and I'll get back to you in the next 48 hours.
+              and I'll get back to you in the next{" "}
+              <span className="emphasis"> 48 hours. </span>
             </h3>
             {/* Links */}
             <form>
@@ -36,33 +71,22 @@ function Contact() {
               </button>
             </form>
           </div>
-          <div className="contact-message">
+          <div className="contact-details">
             <h3>Or you can reach out through these platforms</h3>
-            <div className={`social--links`} id="social--links">
-              <a
-                href="https://www.linkedin.com/in/oluwatobi-ojo-samuel/"
-                target="_blank"
-                className="social--link"
-              >
-                <LinkedIn />
-              </a>
-              <a
-                href="https://twitter.com/OluwatobiOjo5"
-                target="_blank"
-                className="social--link"
-              >
-                <Twitter />
-              </a>
-              <a
-                href="https://github.com/ohteesss"
-                target="_blank"
-                className="social--link"
-              >
-                <Github />
-              </a>
+            <div className="contact-email">
+              <div className="email-icon">
+                <TfiEmail />
+              </div>
+              <p>oluwatobiojo2911@gmail.com</p>
+            </div>
+            <div className="social--links" id="social--links">
+              {socialLinks.map((link, index) => (
+                <SocialLink key={index} link={link.link} icon={link.icon} />
+              ))}
             </div>
           </div>
         </div>
+        <h3 className="copyright">&copy; Daniel Ayodeji {year} </h3>
       </div>
     </section>
   );
