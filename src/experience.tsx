cbf92696699e0d0da907fbd8ceefc8bd926333dd
Ojo <oluwatobiojo2911@gmail.com>
2024-09-30 00:54:12 +0100

    Refactor experience.tsx and project.tsx to use SectionHeader component for header

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,17 +1,15 @@
 import { experiences } from "./app.constants";
 import BackgroundGradient from "./components/ui/backgorund-gradient";
 import ExperienceComponent from "./components/ui/experience";
-import FullStop from "./components/ui/full-stop";
+import SectionHeader from "./components/ui/section-header";
 
 function Experience() {
   return (
     <section className="experience-section" id="experience">
       <BackgroundGradient className="experience-background-gradient" />
       <div className="container">
-        <div className="header">
-          {" "}
-          <h1>Experience</h1> <FullStop />
-        </div>
+        <SectionHeader text="Experience" />
+
         <div className="experience-box">
           {experiences.map((experience, i) => (
             <ExperienceComponent
--- src/project.tsx
+++ src/project.tsx
@@ -1,15 +1,12 @@
 import { projects } from "./app.constants";
-import FullStop from "./components/ui/full-stop";
 import ProjectItem from "./components/ui/project-item";
+import SectionHeader from "./components/ui/section-header";
 
 function Project() {
   return (
     <section className="project-section" id="project">
       <div className="container">
-        <div className="header">
-          {" "}
-          <h1>Projects</h1> <FullStop />
-        </div>
+        <SectionHeader text="Projects" />
         <div className="project-box">
           {projects.map((project, i) => (
             <ProjectItem
