823aa6b36caefc1dfec8bae769915d3dbcaca344
Ojo <oluwatobiojo2911@gmail.com>
2024-09-30 00:53:32 +0100

    Refactor contact.tsx to add form submission functionality and update component structure

--- src/contact.tsx
+++ src/contact.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { TfiEmail } from "react-icons/tfi";
 import { socialLinks } from "./app.constants";
-import FullStop from "./components/ui/full-stop";
-import SocialLink from "./components/ui/social-link";
 import BackgroundGradient from "./components/ui/backgorund-gradient";
+import SectionHeader from "./components/ui/section-header";
+import SocialLink from "./components/ui/social-link";
+import { TypewriterEffectSmooth } from "./components/ui/typewriter";
+import emailjs from "@emailjs/browser";
+import { envVars } from "./env-vars";
+import { BiLoaderAlt } from "react-icons/bi";
 
 function Contact() {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useRef<HTMLFormElement>(null);
   const handleHover = (value: number, e: MouseEvent) => {
     if ((e.target as HTMLElement).closest(".social--link")) {
       const link = (e.target as HTMLElement).closest(
@@ -37,16 +43,39 @@ function Contact() {
     }
   }, []);
 
+  console.log(isLoading);
+
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs
+      .sendForm(
+        envVars.REACT_APP_EMAIL_SERVICE_ID,
+        envVars.REACT_APP_EMAIL_TEMPLATE_ID,
+        form.current!,
+        {
+          publicKey: envVars.REACT_APP_PUBLIC_KEY,
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setIsLoading(false);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setIsLoading(false);
+        }
+      );
+  };
+
   const year = new Date().getFullYear();
 
   return (
     <section className="contact-section" id="contact">
       <BackgroundGradient className="contact-background-gradient" />
       <div className="container">
-        <div className="header">
-          {" "}
-          <h1>Get in Touch</h1> <FullStop />
-        </div>
+        <SectionHeader text="Get in Touch" />
         <div className="contact-box">
           <div className="contact-message">
             <h3>
@@ -55,26 +84,55 @@ function Contact() {
               <span className="emphasis"> 48 hours. </span>
             </h3>
             {/* Links */}
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <div className="form-group">
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" required />
               </div>
               <div className="form-group">
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" required />
               </div>
               <div className="form-group">
-                <label htmlFor="message">Message</label>
-                <textarea id="message" name="message"></textarea>
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" required></textarea>
               </div>
               <button type="submit" className="contact-btn">
-                Send
+                {!isLoading && "Send"}
+                {isLoading && <BiLoaderAlt />}
               </button>
             </form>
           </div>
           <div className="contact-details">
-            <h3>Or you can reach out through these platforms</h3>
+            {/* <h3>Or you can reach out through these platforms</h3> */}
+            <TypewriterEffectSmooth
+              words={[
+                {
+                  text: "Or",
+                },
+                {
+                  text: "you",
+                },
+                {
+                  text: "can",
+                },
+                {
+                  text: "reach",
+                },
+                {
+                  text: "out",
+                },
+                {
+                  text: "through",
+                },
+                {
+                  text: "these",
+                },
+                {
+                  text: "platforms",
+                },
+              ]}
+            />
             <div className="contact-email">
               <div className="email-icon">
                 <TfiEmail />
