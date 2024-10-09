3b03c0d91784d48a1292b42249c7b7b78fc80c6c
Ojo <oluwatobiojo2911@gmail.com>
2024-10-10 00:56:55 +0100

    Refactor Experience component to add sticky navigation on scroll

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,11 +1,30 @@
+import { useRef, useEffect } from "react";
 import { experiences } from "./app.constants";
 import BackgroundGradient from "./components/ui/backgorund-gradient";
 import ExperienceComponent from "./components/ui/experience";
 import SectionHeader from "./components/ui/section-header";
 
 function Experience() {
+  const ref = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          console.log("Intersecting");
+          document.querySelector("nav")!.style.position = "sticky";
+
+          observer.unobserve(ref.current!);
+        }
+      },
+      { rootMargin: "100px" }
+    );
+
+    observer.observe(ref.current!);
+
+    return () => observer.disconnect();
+  }, []);
   return (
-    <section className="experience-section" id="experience">
+    <section className="experience-section" ref={ref} id="experience">
       <BackgroundGradient className="experience-background-gradient" />
       <div className="container">
         <SectionHeader text="Experience" />
