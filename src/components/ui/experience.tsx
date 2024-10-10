7666b273b4b5de25997f15e7445e271c1dd1e703
Ojo <oluwatobiojo2911@gmail.com>
2024-10-10 01:00:38 +0100

    Refactor ExperienceComponent to add SplitText component and update description rendering

--- src/components/ui/experience.tsx
+++ src/components/ui/experience.tsx
@@ -1,4 +1,5 @@
 import { ExperienceType } from "../../app.interface";
+import { SplitText } from "./split-text";
 
 interface Experience extends ExperienceType {
   index: number;
@@ -12,23 +13,53 @@ function ExperienceComponent({
 }: Experience) {
   return (
     <div className="experience">
-      <span className="experience-number">0{index}</span>
+      <div>
+        <span className="experience-number">0{index}</span>
+        {/* <motion.div
+          className="experience-bar"
+          // style={{ scaleY: scrollYProgress }}
+        ></motion.div> */}
+      </div>
+
       <div className="experience-info">
         <h6>{role || "Role"}</h6>
         <h3>{companyName || "Company Name"}</h3>
-        <p>
+        <SplitText
+          text={
+            description ||
+            `description: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Debitis praesentium natus possimus expedita voluptatum adipisci
+          similique sed ipsa consequatur, excepturi asperiores vitae neque.
+          Laborum quod optio illo nulla magnam? Neque est, inventore ad dolorum
+          omnis veniam fugit consectetur vero itaque!`
+          }
+          delay={10}
+          transforms={["translate3d(0,0,0)"]}
+        />
+        {/* <p>
           {description ||
             `description: Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Debitis praesentium natus possimus expedita voluptatum adipisci
           similique sed ipsa consequatur, excepturi asperiores vitae neque.
           Laborum quod optio illo nulla magnam? Neque est, inventore ad dolorum
           omnis veniam fugit consectetur vero itaque!`}
-          <br />
-          <br />
+        </p> */}
+        <br />
+        <br />
+        <SplitText
+          text={`description: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Debitis praesentium natus possimus expedita voluptatum adipisci
+          similique sed ipsa consequatur, excepturi asperiores vitae neque.
+          Laborum quod optio illo nulla magnam? Neque est, inventore ad dolorum
+          omnis veniam fugit consectetur vero itaque!`}
+          delay={10}
+          transforms={["translate3d(0,0,0)"]}
+        />
+        {/* <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
           quasi porro quod rerum dignissimos laudantium at cumque vel?
           Reprehenderit, itaque!
-        </p>
+        </p> */}
       </div>
     </div>
   );
