a6c36c335d96bcdc361b8580fb54f9df6aa94456
Ojo <oluwatobiojo2911@gmail.com>
2024-10-10 01:01:08 +0100

    Refactor SplitText component to support sequential application of transforms

--- src/components/ui/split-text.tsx
+++ src/components/ui/split-text.tsx
@@ -5,10 +5,12 @@ export const SplitText = ({
   text,
   className = "",
   delay = 100,
+  transforms = ["translate3d(0,-10px,0)", "translate3d(0,0,0)"],
 }: {
   text: string;
   className?: string;
   delay?: number;
+  transforms?: string[];
 }) => {
   const letters = text.split("");
   const [inView, setInView] = useState(false);
@@ -30,17 +32,22 @@ export const SplitText = ({
     return () => observer.disconnect();
   }, []);
 
+  const applyTransformsSequentially = async (
+    next: (arg0: { opacity: number; transform: string }) => void,
+    transforms: string[]
+  ) => {
+    for (const transform of transforms) {
+      await next({ opacity: 1, transform });
+    }
+  };
+
   const springs = useSprings(
     letters.length,
     letters.map((_, i) => ({
       from: { opacity: 0, transform: "translate3d(0,40px,0)" },
       to: inView
-        ? async (
-            next: (arg0: { opacity: number; transform: string }) => void
-          ) => {
-            await next({ opacity: 1, transform: "translate3d(0,-10px,0)" });
-            await next({ opacity: 1, transform: "translate3d(0,0,0)" });
-          }
+        ? (next: (arg0: { opacity: number; transform: string }) => void) =>
+            applyTransformsSequentially(next, transforms)
         : { opacity: 0, transform: "translate3d(0,40px,0)" },
       delay: i * delay,
     }))
